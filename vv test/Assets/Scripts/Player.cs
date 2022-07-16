75875f0e414196a6006785a6f4804bf712318e2e
JoseZacharia <105496485+JoseZacharia@users.noreply.github.com>
2022-07-16 17:33:34 +0530

    slo mo + shield prefab w invincibility layer

--- vv test/Assets/Scripts/Player.cs
+++ vv test/Assets/Scripts/Player.cs
@@ -8,21 +8,62 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 playerDirection;
 
+    public float sloMoTime, shieldTime;
+
+    float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.layer = 8;
+        Time.timeScale = 1;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float diectionY = Input.GetAxisRaw("Vertical");
-        playerDirection = new Vector2(0, diectionY).normalized;
+        //float directionY = Input.GetAxisRaw("Vertical");
+        //playerDirection = new Vector2(0, directionY).normalized;
+
+        verticalInput = Input.GetAxis("Vertical");
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(0, playerDirection.y * playerSpeed);          
+        rb.velocity = new Vector2(0, verticalInput * playerSpeed);
+        //rb.velocity = new Vector2(0, playerDirection.y * playerSpeed);          
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Slo Mo")
+        {
+            Destroy(collision.gameObject);
+            StartCoroutine(sloMoEffect());
+
+        }
+
+        if(collision.tag == "Shield")
+        {
+            Destroy(collision.gameObject);
+            StartCoroutine(shieldEffect());
+        }
+    }
+    
+    IEnumerator sloMoEffect()
+    {
+        Time.timeScale = 0.5f;
+        yield return new WaitForSeconds(sloMoTime);
+        Time.timeScale = 1;
+    }
+
+    IEnumerator shieldEffect()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Invincibility");
+        GetComponent<SpriteRenderer>().color = new Color(0.53f, 1f, 1f, 0.5f);
+        yield return new WaitForSeconds(shieldTime);
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        yield return new WaitForSeconds(1);
+        gameObject.layer = LayerMask.NameToLayer("Player");
     }
 }
